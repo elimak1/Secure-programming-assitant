924f5f0e75b3e76945d26275f559bb91a196a571
elias <elias.makela123@gmail.com>
2024-03-28 14:55:34 +0200

    add sorting to conversations table

--- secure-programming-assistant-ui/src/app/chats/chats.component.ts
+++ secure-programming-assistant-ui/src/app/chats/chats.component.ts
@@ -1,46 +1,62 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild} from '@angular/core'
 import { AuthService } from '@auth0/auth0-angular'
 import { HttpService } from '../services/http.service'
 import { Router } from '@angular/router'
 import { Message } from '../../models/types'
 import { SpinnerComponent } from '../spinner/spinner.component'
-import { MatTableModule } from '@angular/material/table'
 import { CommonModule } from '@angular/common'
 import moment from 'moment'
 import { FormatMessageComponent } from '../format-message/format-message.component'
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-chats',
   standalone: true,
-  imports: [SpinnerComponent, MatTableModule, CommonModule, FormatMessageComponent],
+  imports: [SpinnerComponent, MatTableModule, 
+    CommonModule, FormatMessageComponent, MatSortModule],
   templateUrl: './chats.component.html',
   styleUrl: './chats.component.scss'
 })
 export class ChatsComponent {
   public isLoading: boolean = true
-  public oldChats: Message[] = []
   public moment = moment
+
+  public dataSource: MatTableDataSource<Message> = new MatTableDataSource<Message>([])
 
   constructor(
     private authService: AuthService,
     private httpService: HttpService,
     private router: Router
-  ) {}
+  ) {
+  }
+
+  @ViewChild(MatSort,{static:false}) sort?: MatSort;
+
+  ngAfterViewInit() {
+    console.log(this.sort)
+    if (this.sort) {
+      this.dataSource.sort = this.sort;
+    }
+  }
 
   ngOnInit() {
     this.authService.user$.subscribe((user) => {
       if (user) {
         this.httpService.getChats().subscribe((chats) => {
-          this.oldChats = chats
+          this.dataSource.data = chats
           this.isLoading = false
         })
       }
     })
   }
 
-  deleteChat(chatId: string) {
+  deleteChat(chatId: string, event: MouseEvent) {
+    event.stopPropagation();
     this.httpService.deleteChat(chatId).subscribe(() => {
-      this.oldChats = this.oldChats.filter((chat) => chat.chatId !== chatId)
+      this.dataSource.data = this.dataSource.data.filter(
+        (chat: Message) => chat.chatId !== chatId
+      )
     })
   }
 
